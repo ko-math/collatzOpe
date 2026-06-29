b1f16448194a7001b66cd119aa54b659710cad5f
ko-math <sorehakiemasu@gmail.com>
2026-06-29 15:07:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-/*
-const N = document.querySelector('#inp').value;
-const col = document.querySelector('#col');
-for(let n = 1;n <= N;n++){
-  const arrays = Collatz(n);
-  const p = document.createElement('p'); 
-  let arr = '';
-  for(let i = 0;i < arrays[0].length;i++){
-    const add = arrays[0][i];
-    if(arrays[1].includes(add)){
-      arr += add;
-    } else {
-      arr += '<b>' + add + '</b>';
+const button = document.querySelector('#but');
+but.addEventListener('click',function(){
+  const N = document.querySelector('#inp').value;
+  const col = document.querySelector('#col');
+  for(let n = 1;n <= N;n++){
+    const arrays = Collatz(n);
+    const p = document.createElement('p'); 
+    let arr = '';
+    for(let i = 0;i < arrays[0].length;i++){
+      const add = arrays[0][i];
+      if(arrays[1].includes(add)){
+        arr += add;
+      } else {
+        arr += '<b>' + add + '</b>';
+      }
+      arr += ',';
     }
-    arr += ',';
+    p.innerHTML = arr;
+    col.append(p);
   }
-  p.innerHTML = arr;
-  col.append(p);
-}
+});
 
-function Collatz(N) {
+function Collatz(N){
     const Seq = [N];
     const Mer = [];
     let n = N;
     while(1){
-        if (n === 1) break;
-        if (n % 2 === 0) {
+        if(n === 1) break;
+        if(n % 2 === 0){
             n /= 2;
         } else {
             n = (3 * n + 1) / 2;
@@ -34,4 +36,3 @@ function Collatz(N) {
     }
     return [Seq,Mer];
 }
-*/
